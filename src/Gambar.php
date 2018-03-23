9ab41086c5e728080d92968924ea9385ffa2ae04
oji <oji@riau.go.id>
2018-03-23 11:02:51 +0700

    fix bug

--- src/Gambar.php
+++ src/Gambar.php
@@ -3,25 +3,21 @@ namespace OjiSatriani;
 
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
-// use Storage;
+
 class Gambar
 {
     private $jenis;
     private $tersimpan;
-    //private $destinationPath;
-    //private $nama;
-
-    //function __set($propName, $propValue)
-    //{
-        //$this->$propName = $propValue;
-        //property dipakai ( jenis, destinationPath, nama)
-    //}
+    private $destinationPath;
+    private $nama;
 
     public function __construct($path, $nama, $file)
     {
-        if(Storage::put($target . $nama, file_get_contents($file)))
+        if(Storage::put($path . $nama, file_get_contents($file)))
         {
-            $this->tersimpan = TRUE;
+            $this->destinationPath  = storage_path().'/app/'.$path;
+            $this->nama             = $nama;
+            $this->tersimpan        = TRUE;
             return TRUE;
         } else {
             $this->tersimpan = FALSE;
@@ -34,50 +30,39 @@ class Gambar
         return new static($path, $nama, $file);
     }
 
-    public function ukuranBaru(array $ukuran = null){
-        $ukuran = $ukuran === null ? ['wide','banner']:$ukuran;
+    public function ukuranBaru(array $ukuran = []){
         foreach($ukuran as $value){
-            // $this->ukuranGambar($value);
-            //$this->nama =
-            //$this->jenis = $value;
-            $gbr = $this->ukuranData();
-            $img = Image::make($destinationPath .'/'. $nama);
+            $gbr = self::ukuranData();
+            $img = Image::make($this->destinationPath .'/'. $this->nama);
             $img->resize($gbr[$value]['lebar'], $gbr[$value]['tinggi']);
-            $img->save($destinationPath .'/'. $this->jenis .'-'. $nama);
+            $img->save($this->destinationPath .'/'. $value .'-'. $this->nama);
             $img->destroy();
         }
     }
 
-    public function hapus($path, $file)
+    public static function hapus($fullPathToFile)
     {
-        $path = 'app/'. $path .'/';
-        if (!empty($file)) {
-            if (file_exists($path . $file)) {
-                //unlink($path . $file);
-                Storage::delete($path . $file);
-            }
-            // $tipe = array("kecil", "thumb", "empatEnam", "sedang", "wide", "slide", "banner","avatar");
-            foreach ($this->ukuranData() as $gbr) {
-                // $exists = Storage::disk('local')->exists('file.jpg');
-                // if (file_exists($path . $gbr .'-'. $file)) {
-                if (Storage::disk('local')->exists($path . $file)) {
-                    // unlink($path . $gbr .'-'. $file);
-                    Storage::delete($path . $gbr .'-'. $file);
+        if (file_exists($fullPathToFile)) {
+            if(unlink($fullPathToFile))
+            {
+                foreach (self::ukuranData() as $key => $value) {
+                    $data = explode('/', $fullPathToFile);
+                    $file = end($data);
+                    $filter = array_filter($data);
+                    array_pop($filter);
+                    $path = implode("/",$filter);
+                    if (file_exists($path .'/'. $key .'-'. $file)) {
+                        unlink($path .'/'. $key .'-'. $file);
+                    }
                 }
+            } else {
+                return false;
             }
         }
+        return true;
     }
 
-    public function tipe($tipe){
-        if($tipe !="image/gif" and	$tipe !="image/jpeg" and $tipe !="image/pjpeg" and $tipe !="image/png") return false;
-        else return true;
-    }
-
-    public function ukuranGambar($jenis){
-        $this->jenis = $jenis;
-    }
-
-    public function ukuranData(){
+    public static function ukuranData(){
         return [
             'thumb'=>[
                 'lebar'     => 180,
@@ -87,47 +72,11 @@ class Gambar
                 'lebar'     => 508,
                 'tinggi'    => 377,
             ],
+            'avatar'=>[
+                'lebar'     => 128,
+                'tinggi'    => 128,
+            ],
         ];
     }
-
-    // public function getLebar(){
-    //     $lebar = 700;
-    //     $lebar = $this->jenis == 'empatEnam' ? 180:$lebar;
-    //     $lebar = $this->jenis == 'thumb' ? 280:$lebar;
-    //     $lebar = $this->jenis == 'sedang' ? 330:$lebar;
-    //     $lebar = $this->jenis == 'kecil' ? 80:$lebar;
-    //     $lebar = $this->jenis == 'wide' ? 508:$lebar;
-    //     $lebar = $this->jenis == 'slide' ? 1020:$lebar;
-    //     $lebar = $this->jenis == 'banner' ? 300:$lebar;
-    //     $lebar = $this->jenis == 'avatar' ? 128:$lebar;
-    //     return $lebar;
-    // }
-
-    // public function getTinggi(){
-    //     $tinggi = 500;
-    //     $tinggi = $this->jenis == 'empatEnam' ? 250:$tinggi;
-    //     $tinggi = $this->jenis == 'thumb' ? 280:$tinggi;
-    //     $tinggi = $this->jenis == 'sedang' ? 240:$tinggi;
-    //     $tinggi = $this->jenis == 'kecil' ? 80:$tinggi;
-    //     $tinggi = $this->jenis == 'wide' ? 377:$tinggi;
-    //     $tinggi = $this->jenis == 'slide' ? 450:$tinggi;
-    //     $tinggi = $this->jenis == 'banner' ? 100:$tinggi;
-    //     $tinggi = $this->jenis == 'avatar' ? 128:$tinggi;
-    //     return $tinggi;
-    // }
-
-    // public function namaBaru(){
-    //         return $this->jenis;
-    // }
-
-    
-
-    // public function tempelBarcode($jenisGbr, $barcodePath){
-    //     $img = Image::make($this->destinationPath .'/'. $jenisGbr .'-'. $this->nama);
-    //     //$watermark = Image::make($barcodePath);
-    //     //$img->insert($watermark, 'center');
-    //     $img->insert($barcodePath, 'bottom-right', 10, 10);
-    //     $img->save($this->destinationPath .'/'. $this->jenis .'-'. $this->nama);
-    // }
 
 }
